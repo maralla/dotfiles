3d7a73fc9f6f0126eda5b92c4a80bd27a9263814
maralla <maralla.ai@gmail.com>
2018-06-03 14:23:55 +0800

    Install mpv config

--- install.sh
+++ install.sh
@@ -9,9 +9,11 @@ git clone https://github.com/maralla/dotfiles.git ~/.dotfiles || exit 1
 echo "unlinking old files ..."
 rm -v ~/.tmux.conf
 rm -v ~/.gitconfig
+rm -v ~/.config/mpv
 rm -vrf ~/.pip
 
 echo "linking new files ..."
 ln -vs ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -vs ~/.dotfiles/git/gitconfig ~/.gitconfig
 ln -vs ~/.dotfiles/pip ~/.pip
+ln -vs ~/.dotfiles/mpv ~/.config/mpv
