5d2eacbca85251d1351186789428908d08ce90df
maralla <maralla.ai@gmail.com>
2020-11-15 13:33:09 +0800

    Renamed tools script file name

--- tools.sh
+++ tools.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+# Go tools
 
 GOPATH=$HOME/Workspace/app
+GO111MODULE="on"
 
 echo Install gocode
 go get -u github.com/stamblerre/gocode
-
-export GO111MODULE="on"
 
 echo Install goimports
 go get golang.org/x/tools/cmd/goimports
@@ -18,3 +21,18 @@ go get golang.org/x/tools/cmd/gopls
 
 echo Install guru
 go get golang.org/x/tools/cmd/guru
+
+
+# System tools
+
+if ! command -v delta &> /dev/null
+then
+    echo Install delta for git
+    brew install git-delta
+fi
+
+if ! command -v gpg &> /dev/null
+then
+    echo Install gnupg
+    brew install gnupg
+fi
